323a54f6a4fc9b71cd11222dac8f7f5ff426b5c9
oracle <oracle@localhost>
2020-03-02 14:33:33 +0100

    Finalizado el trabajo con ObjectDb

--- src/pvehiculos/Pvehiculos.java
+++ src/pvehiculos/Pvehiculos.java
@@ -5,6 +5,10 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import org.bson.Document;
  // Ejercicio DE EXAMEN
         // Se emplean oracle, OBJECTDB Y MONGO
@@ -22,10 +26,25 @@ public class Pvehiculos {
     
     /*
     VARIABLES CAMPO DE MONGO
+    Tres Variables tipo normales
+    Todas static, asi solo se crea una vez para evitar confusión
     */
     public static double id;
     public static String dni;
     public static String codveh;
+    
+    /*
+    VARIABLE CREADAS PARA OBJECTDB
+    UN Objeto de TIPO Clientes
+    */
+    static Clientes clienteDat;
+    static String nomeCli;
+    static int comprasCli;
+    static Vehiculos vehiculosDat;
+    static String nomveh;
+    static int prezoorixe;
+    static int anomatricula;
+    
     
    
     public void conexionMongo(){
@@ -51,6 +70,56 @@ public class Pvehiculos {
             id = doc.getDouble("_id");
             dni = doc.getString("dni");
             codveh = doc.getString("codveh");
+            
+            
+    // Ahora accedemos a objectDB??
+            /*
+            Creamos un EntityManagerFactory
+            Como es un metodo perteneciente a la clase Persistence y es no es static
+            Se hace referencia directamente a la clase a la que pertenece
+            De parametro necesita la unidad Persistence a la que se quiere acceder
+            */
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("$objectdb/db/vehicli.odb");
+            EntityManager em = emf.createEntityManager();
+            
+            //Sacamso el nombre y compras de los Clientes de la clase Clientes.class
+            
+            /* 
+            Uso de la interfaz Transiction
+            Begin nos permite comenzarla
+            */
+            em.getTransaction().begin();
+            TypedQuery<Clientes> query = em.createQuery("select p from Clientes p where p.dni = :value", Clientes.class);
+            clienteDat = query.setParameter("value", dni).getSingleResult();
+            
+            // Esto nos permite almacenar nome y compras de un cliente
+            nomeCli = clienteDat.getNomec();
+            comprasCli = clienteDat.getNcompras();
+            
+            em.getTransaction().commit();
+            
+            /*
+            Ahora se realizara lo mismo pero con las class Vehiculos
+            */
+            
+            em.getTransaction().begin();
+            TypedQuery<Vehiculos> query2 = em.createQuery("select v from Vehiculos v where v.codveh= :value2", Vehiculos.class);
+            vehiculosDat = query2.setParameter("value2", codveh).getSingleResult();
+            
+            nomveh = vehiculosDat.getNomveh();
+            prezoorixe = vehiculosDat.getPrezoorixe();
+            anomatricula = vehiculosDat.getAnomatricula();
+            
+            em.getTransaction().commit();
+            /*
+            Cerramos conexion con todos los objetod Entity creados ya que no necesitamos sacar mas datos de nuestras bases de datos
+            A partir de aqui ya tenemos que introducir los datos de nuestras variables que sacamos de ObjectDb
+            Iniciando la conexion con oracle
+            */
+            
+            em.close();
+            emf.close();
+            
             
         }
         
